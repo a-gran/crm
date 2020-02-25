a4b079dcb56d08a3d3afaaac4293ac1b392680f1
awebdeff <awebdeff@gmail.com>
2020-02-25 22:08:59 +0300

    start of development and server authorization

---
+++ controllers/auth.js
@@ -0,0 +1,64 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.login = async function(req, res) {
+  const candidate = await User.findOne({ email: req.body.email })
+
+  if (candidate) {
+    // проверка пароля => пользователь существует
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      // Генерация токена => пароли совпали
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, { expiresIn: 3600 })
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      // пароли НЕ совпали => сообщение
+      res.status(401).json({
+        message: 'Пароли не совпадают. Попробуйте еще раз'
+      })
+    }
+  } else {
+    // пользователь НЕ существует => сообщение
+    res.status(404).json({
+      message: 'Пользователь с таким email не найден.'
+    })
+  }
+}
+
+module.exports.register = async function (req, res) {
+  // email, password, => create accaunt
+  const candidate = await User.findOne({ email: req.body.email })
+  
+  if (candidate) {
+    // Пользователь существует => отдать ошибку
+    res.status(409).json({
+      message: 'Пользователь с таким email уже зарегистрирован. Попробуйте другой.'
+    })
+  } else {
+    // Пользователь НЕ существует => создать пользователя
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    })
+
+    try {
+      await user.save()
+      res.status(201).json(user)
+    } catch(e) {
+      errorHandler(res, e)
+    }
+    
+  }
+}
+
